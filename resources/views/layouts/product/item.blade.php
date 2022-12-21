7b5ee80aec98361e7595978de19fbc1f42151273
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2022-12-21 23:34:53 +0700

    fixed

--- resources/views/layouts/product/item.blade.php
+++ resources/views/layouts/product/item.blade.php
@@ -5,7 +5,7 @@ $img = json_decode($pro->images);
 <div class="services2 mb-60">
 <div class="services2-img"><img src="{{$img[0]}}" alt="" class="w-100"></div>
 <div class="services2-content">
-      <div class="icon"><img src="{{url('frontend/images/icon-1.png')}}" alt=""></div>
+      {{-- <div class="icon"><img src="{{url('frontend/images/icon-1.png')}}" alt=""></div> --}}
       <h3 class="services2-title">{{languageName($pro->name)}}</h3>
       <p class="services2-text">{!!languageName($pro->description)!!}</p>
       <div class="butn-dark mt-20"> <a href="{{route('detailProduct',['cate'=>$pro->cate_slug,'type'=>$pro->type_slug ? $pro->type_slug : 'loai','id'=>$pro->slug])}}"><span>Chi tiết</span></a> </div>
