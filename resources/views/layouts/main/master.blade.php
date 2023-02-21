a46288aeb57d6c314215549469346f3bce2fac25
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-02-21 09:07:12 +0700

    fixed

--- resources/views/layouts/main/master.blade.php
+++ resources/views/layouts/main/master.blade.php
@@ -31,8 +31,22 @@
       <link rel="stylesheet" href="{{asset('frontend/css/plugins.css')}}" />
       <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}" />
       @yield('css')
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TMR2TXW');</script>
+      <!-- End Google Tag Manager -->
    </head>
    <body>
+      <!-- Google Tag Manager -->
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-TMR2TXW');</script>
+      <!-- End Google Tag Manager -->
       <!-- Preloader -->
       <div id="preloader"></div>
       <!-- Progress scroll totop -->
